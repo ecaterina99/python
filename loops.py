761a84c6b8fed5006d47539f07c69d2a1123ab63
ecaterina99 <tricolicikatea1999@gmail.com>
2024-10-22 23:33:16 +0300

    basic exercise

---
+++ loops.py
@@ -0,0 +1,8 @@
+i=0
+while i<3:
+  print("meow")
+  i += 1
+for i in [0,1,2]:
+  print("hello world")
+for _ in range(9):
+  print("!")
---
+++ uppercase.py
@@ -0,0 +1,10 @@
+before = input("Before: ")
+print("After: ", end="")
+print(before.upper())
+
+before = input("Before: ")
+after=before.upper()
+print(f"After: {after}")
+
+before = input("Before: ")
+print(f"After: {before.upper()}")
